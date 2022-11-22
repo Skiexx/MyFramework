61a0c1bea95198b719198033055c70bce9e56b36
skiexx <codeskiexx@yandex.ru>
2022-11-22 09:38:54 +0700

    Added requirements to composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,19 +1,7 @@
 <?php
 const DIR_CONFIG = '/../config';
 
-spl_autoload_register(function ($className) {
-    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
-    $className = str_replace('\\', '/', $className);
-
-    foreach ($paths['classes'] as $path) {
-        $file = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
-
-        if (file_exists($file)) {
-            require_once $file;
-            return;
-        }
-    }
-});
+require_once __DIR__ . '/../vendor/autoload.php';
 
 function getConfigs(string $path = DIR_CONFIG): array
 {
